dfb6018800fb7973de5ba74cf5987e641d328654
Elias John Sabu <147395622+eliasurk22@users.noreply.github.com>
2025-07-25 14:42:03 +0530

    Update db_loader.py

--- backend/database/db_loader.py
+++ backend/database/db_loader.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import os
 
-# Load CSV dataset
 def load_recipes(csv_path = os.path.join(os.path.dirname(__file__), "recipes.csv")):
     df = pd.read_csv(csv_path)
     required_columns = {"Title", "Ingredients", "Instructions", "Image_Name", "Cleaned_Ingredients"}
@@ -11,5 +10,5 @@ def load_recipes(csv_path = os.path.join(os.path.dirname(__file__), "recipes.csv
     
     return df
 
-# Load data once and keep in memory for efficiency
+# it makes more sense to just load the database once soooo
 recipe_df = load_recipes()
